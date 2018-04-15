e074518a5df4c479158b9f3e2b4df228c6323d64
Edilson Ngulele <edilsonngulele@gmail.com>
2018-04-15 22:47:00 +0200

    first commit

---
+++ Speech-to-Text/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Speech-to-Text
+//
+//  Created by Edilson Ngulele on 4/15/18.
+//  Copyright © 2018 Edilson Ngulele. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Speech-to-TextTests/Speech_to_TextTests.swift
@@ -0,0 +1,36 @@
+//
+//  Speech_to_TextTests.swift
+//  Speech-to-TextTests
+//
+//  Created by Edilson Ngulele on 4/15/18.
+//  Copyright © 2018 Edilson Ngulele. All rights reserved.
+//
+
+import XCTest
+@testable import Speech_to_Text
+
+class Speech_to_TextTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
